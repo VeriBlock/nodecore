8defeff19b4e184130842c45951dd5f18b78c0e4
PCasafont <pere@pcasafont.net>
2019-08-27 10:48:00 +0200

    Final teamcity settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,13 @@
-import jetbrains.buildServer.configs.kotlin.v2018_2.*
+
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.sshAgent
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -25,4 +34,88 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2019.1"
 
 project {
+
+    buildType(Snapshot)
+    buildType(ReleaseCandidate)
+    buildType(FinalRelease)
+
+    params {
+        param("env.GIT_BRANCH", "unused") // TODO remove once we get rid of this parameter on the root project
+    }
 }
+
+object Snapshot : Build(
+    name = "Snapshot",
+    gradleTasks = "devSnapshot build installDist",
+    branchFilter = "+:refs/heads/master",
+    artifactoryRepoKey = "libs-snapshot-local"
+)
+
+object ReleaseCandidate : Build(
+    name = "Release Candidate",
+    gradleTasks = "candidate build installDist",
+    branchFilter = """+:refs/heads/release/*""",
+    artifactoryRepoKey = "libs-release-local"
+)
+
+object FinalRelease : Build(
+    name = "Final Release",
+    gradleTasks = "final build installDist",
+    branchFilter = null,
+    artifactoryRepoKey = "libs-release-local"
+)
+
+abstract class Build(
+    name: String,
+    gradleTasks: String,
+    branchFilter: String?,
+    artifactoryRepoKey: String
+) : BuildType({
+    this.name = name
+
+    artifactRules = """
+        nodecore-cli/build/distributions => nodecore-cli
+        nodecore-miners-pop/build/distributions => nodecore-miners-pop
+        nodecore-miners-pow/build/distributions => nodecore-miners-pow
+    """.trimIndent()
+
+    params {
+        param("system.artifactory_repoKey", artifactoryRepoKey)
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            tasks = gradleTasks
+            buildFile = "build.gradle"
+            gradleParams = "%gradle.cli.params%"
+        }
+    }
+
+    if (branchFilter != null) {
+        triggers {
+            vcs {
+                enabled = true
+                this.branchFilter = branchFilter
+                perCheckinTriggering = true
+                groupCheckinsByCommitter = true
+                enableQueueOptimization = false
+            }
+        }
+    }
+
+    features {
+        sshAgent {
+            teamcitySshKey = "github"
+            param("secure:passphrase", "credentialsJSON:d9d1fd02-aa80-4456-9f17-43a2354af355")
+        }
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_7"
+            }
+        }
+    }
+})
