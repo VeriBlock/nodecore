0c93042fca541f6f64d2d3b75cd827e82a61bd70
PCasafont <pere@pcasafont.net>
2019-10-30 09:55:35 +0100

    Some kotlinized tests replacing Assert with KotlinTest Assertions and Mockito with Mockk

--- nodecore-miners-pop/src/test/java/nodecore/miners/pop/rules/BlockHeightRuleTests.kt
+++ nodecore-miners-pop/src/test/java/nodecore/miners/pop/rules/BlockHeightRuleTests.kt
@@ -5,195 +5,239 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-package nodecore.miners.pop.rules;
+package nodecore.miners.pop.rules
 
-import nodecore.miners.pop.contracts.Configuration;
-import nodecore.miners.pop.contracts.VeriBlockHeader;
-import nodecore.miners.pop.rules.actions.RuleAction;
-import org.junit.Test;
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import nodecore.miners.pop.contracts.Configuration
+import nodecore.miners.pop.contracts.VeriBlockHeader
+import nodecore.miners.pop.rules.actions.RuleAction
+import org.junit.Test
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
-
-@SuppressWarnings("unchecked")
-public class BlockHeightRuleTests {
+class BlockHeightRuleTests {
     @Test
-    public void evaluateWhenNoConditionsActive() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(false);
+    fun evaluateWhenNoConditionsActive() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns false
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12343
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12339
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12343);
-        when(previous.getHeight()).thenReturn(12339);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, never()).execute(any());
+        // Then
+        verify(exactly = 0) {
+            action.execute(any())
+        }
     }
 
     @Test
-    public void evaluateWhenOnlyKeystoneConditionActive() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(false);
-        when(config.getBoolean("auto.mine.round4")).thenReturn(true);
+    fun evaluateWhenOnlyKeystoneConditionActive() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns false
+            every { getBoolean("auto.mine.round4") } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12343
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12339
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12343);
-        when(previous.getHeight()).thenReturn(12339);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action).execute(12340);
+        // Then
+        verify(exactly = 1) {
+            action.execute(12340)
+        }
     }
 
     @Test
-    public void evaluateWhenAllConditionsActive() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenAllConditionsActive() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12343
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12339
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12343);
-        when(previous.getHeight()).thenReturn(12339);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action).execute(12340);
-        verify(action).execute(12341);
-        verify(action).execute(12342);
-        verify(action).execute(12343);
+        // Then
+        verify(exactly = 1) {
+            action.execute(12340)
+            action.execute(12341)
+            action.execute(12342)
+            action.execute(12343)
+        }
     }
 
     @Test
-    public void evaluateWhenSingleKeystone() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenSingleKeystone() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12340
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12339
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12340);
-        when(previous.getHeight()).thenReturn(12339);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, times(1)).execute(anyInt());
+        // Then
+        verify(exactly = 1) {
+            action.execute(12340)
+        }
     }
 
     @Test
-    public void evaluateWhenSingleRound1() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenSingleRound1() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12341
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12340
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12341);
-        when(previous.getHeight()).thenReturn(12340);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, times(1)).execute(anyInt());
+        // Then
+        verify(exactly = 1) {
+            action.execute(12341)
+        }
     }
 
     @Test
-    public void evaluateWhenSingleRound2() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenSingleRound2() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12342
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12341
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12342);
-        when(previous.getHeight()).thenReturn(12341);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, times(1)).execute(anyInt());
+        // Then
+        verify(exactly = 1) {
+            action.execute(12342)
+        }
     }
 
     @Test
-    public void evaluateWhenSingleRound3() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenSingleRound3() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12343
+        }
+        val previous: VeriBlockHeader = mockk {
+            every { height } returns 12342
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns previous
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
-        VeriBlockHeader previous = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12343);
-        when(previous.getHeight()).thenReturn(12342);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(previous);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, times(1)).execute(anyInt());
+        // Then
+        verify(exactly = 1) {
+            action.execute(12343)
+        }
     }
 
     @Test
-    public void evaluateWhenPreviousNotSet() {
-        RuleAction action = (RuleAction<Integer>)mock(RuleAction.class);
-        Configuration config = mock(Configuration.class);
-        when(config.getBoolean(anyString())).thenReturn(true);
+    fun evaluateWhenPreviousNotSet() {
+        // Given
+        val action: RuleAction<Int?> = mockk(relaxed = true)
+        val config: Configuration = mockk {
+            every { getBoolean(any()) } returns true
+        }
+        val latest: VeriBlockHeader = mockk {
+            every { height } returns 12343
+        }
+        val context: RuleContext = mockk {
+            every { latestBlock } returns latest
+            every { previousHead } returns null
+        }
 
-        VeriBlockHeader latest = mock(VeriBlockHeader.class);
+        // When
+        val rule = BlockHeightRule(action, config)
+        rule.evaluate(context)
 
-        when(latest.getHeight()).thenReturn(12343);
-
-        RuleContext context = mock(RuleContext.class);
-        when(context.getLatestBlock()).thenReturn(latest);
-        when(context.getPreviousHead()).thenReturn(null);
-
-        BlockHeightRule rule = new BlockHeightRule(action, config);
-        rule.evaluate(context);
-
-        verify(action, times(1)).execute(anyInt());
-        verify(action).execute(12343);
+        // Then
+        verify(exactly = 1) {
+            action.execute(12343)
+        }
     }
 }
