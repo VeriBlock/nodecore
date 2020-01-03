8b67cb7cf4a31332e7f065bd7da97001f131d539
PCasafont <pere@pcasafont.net>
2020-01-03 16:32:40 +0100

    Automine config parts as part of general chain config

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/BitcoinFamilyChain.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/BitcoinFamilyChain.kt
@@ -19,7 +19,6 @@ import org.veriblock.core.utilities.extensions.toHex
 import org.veriblock.sdk.AltPublication
 import org.veriblock.sdk.PublicationData
 import org.veriblock.sdk.VeriBlockPublication
-import org.veriblock.sdk.alt.AutoMineConfig
 import org.veriblock.sdk.alt.ChainConfig
 import org.veriblock.sdk.alt.FamilyPluginSpec
 import org.veriblock.sdk.alt.PublicationDataWithContext
@@ -32,8 +31,10 @@ class BtcConfig(
     override val host: String = "http://localhost:8332",
     val username: String? = null,
     val password: String? = null,
-    override val autoMine: AutoMineConfig? = null,
-    val payoutAddress: String? = null
+    val payoutAddress: String? = null,
+    override val keystonePeriod: Int = 5, // TODO: Decide good default value
+    override val blockRoundIndices: IntArray = intArrayOf(3, 1, 2, 1, 2), // TODO: Decide good default value
+    override val autoMineRounds: List<Int> = emptyList()
 ) : ChainConfig()
 
 private data class BtcPublicationData(
