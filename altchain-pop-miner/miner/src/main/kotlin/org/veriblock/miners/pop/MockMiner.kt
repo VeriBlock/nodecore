8dc69cd348a9e5b1fb53f6f0018887a1b852dc9f
PCasafont <pere@pcasafont.net>
2020-01-17 18:07:22 +0100

    Basing mock mining ATV on Alt's last known VBK block

--- altchain-pop-miner/miner/src/main/kotlin/org/veriblock/miners/pop/MockMiner.kt
+++ altchain-pop-miner/miner/src/main/kotlin/org/veriblock/miners/pop/MockMiner.kt
@@ -70,7 +70,11 @@ class MockMiner(
         val publicationData = chain.getPublicationData(block)
 
         val key = KeyGenerator.generate()
-        val atv = mine(publicationData.publicationData, veriBlockBlockchain.chainHead, key)
+        val atv = mine(
+            publicationData.publicationData,
+            SerializeDeserializeService.parseVeriBlockBlock(publicationData.context[0]),
+            key
+        )
 
         val vtb = vpm.mine(
             veriBlockBlockchain.chainHead,
