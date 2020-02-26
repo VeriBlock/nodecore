bc6518edddff5e056285f3a9bae943cd305b0e0f
andrii_kl <andriikliui@gmail.com>
2020-02-26 17:49:34 +0200

    SPV-65 Remove lombok usage

--- nodecore-spv/src/main/java/veriblock/model/DownloadStatusResponse.java
+++ nodecore-spv/src/main/java/veriblock/model/DownloadStatusResponse.java
@@ -1,12 +1,25 @@
 package veriblock.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class DownloadStatusResponse {
     private DownloadStatus downloadStatus;
     private Integer currentHeight;
     private Integer bestHeight;
+
+    public DownloadStatusResponse(DownloadStatus downloadStatus, Integer currentHeight, Integer bestHeight) {
+        this.downloadStatus = downloadStatus;
+        this.currentHeight = currentHeight;
+        this.bestHeight = bestHeight;
+    }
+
+    public DownloadStatus getDownloadStatus() {
+        return downloadStatus;
+    }
+
+    public Integer getCurrentHeight() {
+        return currentHeight;
+    }
+
+    public Integer getBestHeight() {
+        return bestHeight;
+    }
 }
