9f3e62e01b5dd4fcd218e3f6c932c2175169d0cb
Bernat Carbo <bernatgarciacarbo@gmail.com>
2020-02-27 16:56:02 +0100

    getBalance should be able to display the total confirmed and total unconfirmed balances.

--- nodecore-cli/src/main/java/nodecore/cli/serialization/BalancePayload.java
+++ nodecore-cli/src/main/java/nodecore/cli/serialization/BalancePayload.java
@@ -2,19 +2,32 @@ package nodecore.cli.serialization;
 
 import com.google.gson.annotations.SerializedName;
 import nodecore.api.grpc.VeriBlockMessages;
+import org.veriblock.core.utilities.Utility;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BalancePayload {
     public BalancePayload(final VeriBlockMessages.GetBalanceReply reply) {
-        for (final VeriBlockMessages.AddressBalance balanceInfo : reply.getConfirmedList())
+        for (final VeriBlockMessages.AddressBalance balanceInfo : reply.getConfirmedList()) {
             confirmed.add(new AddressBalanceInfo(balanceInfo));
-        for (final VeriBlockMessages.Output output : reply.getUnconfirmedList())
+        }
+        total_confirmed = Utility.formatAtomicLongWithDecimal(reply.getTotalConfirmed());
+        for (final VeriBlockMessages.Output output : reply.getUnconfirmedList()) {
             unconfirmed.add(new OutputInfo(output));
+        }
+        total_unconfirmed = Utility.formatAtomicLongWithDecimal(reply.getTotalUnconfirmed());
     }
+
     @SerializedName("confirmed")
     List<AddressBalanceInfo> confirmed = new ArrayList<>();
+
+    @SerializedName("total_confirmed")
+    public String total_confirmed;
+
     @SerializedName("unconfirmed")
     List<OutputInfo> unconfirmed = new ArrayList<>();
+
+    @SerializedName("total_unconfirmed")
+    public String total_unconfirmed;
 }
