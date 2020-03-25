3150eca525b9415f653e512ca187022ea2dddc9a
BernatCarbo <bernatgarciacarbo@gmail.com>
2020-03-25 17:31:30 +0100

    Increased timeout for Create Bitcoin Endorsement Transaction task

--- nodecore-miners-pop/src/main/java/nodecore/miners/pop/tasks/Tasks.kt
+++ nodecore-miners-pop/src/main/java/nodecore/miners/pop/tasks/Tasks.kt
@@ -64,7 +64,7 @@ suspend fun runTasks(
         operation.runTask(
             taskName = "Create Bitcoin Endorsement Transaction",
             targetState = OperationStateType.ENDORSEMENT_TRANSACTION,
-            timeout = 10.min
+            timeout = 15.min
         ) {
             val miningInstruction = (operation.state as OperationState.Instruction).miningInstruction
             val opReturnScript = bitcoinService.generatePoPScript(miningInstruction.publicationData)
