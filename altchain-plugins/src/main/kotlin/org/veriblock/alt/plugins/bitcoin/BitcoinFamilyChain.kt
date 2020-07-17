ce3eeee6c62d4cedfb4972083afd1cdecaab9660
BernatCarbo <bernatgarciacarbo@gmail.com>
2020-07-17 14:45:07 +0200

    empty check for the payoutAddress configuration

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/bitcoin/BitcoinFamilyChain.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/bitcoin/BitcoinFamilyChain.kt
@@ -70,8 +70,11 @@ class BitcoinFamilyChain(
         checkNotNull(config.payoutAddress) {
             "$name's payoutAddress ($key.payoutAddress) must be configured!"
         }
+        if (config.payoutAddress.isEmpty()) {
+            error("$name's payoutAddress ($key.payoutAddress) shouldn't be empty")
+        }
         if (config.payoutAddress == "INSERT PAYOUT ADDRESS") {
-            error("${config.payoutAddress} is not a valid value for the payoutAddress configuration, please set up a valid payout address")
+            error("${config.payoutAddress} is not a valid value for the $name's payoutAddress configuration ($key.payoutAddress), please set up a valid payout address")
         }
         payoutAddressScript = if (config.payoutAddress.isHex()) {
             config.payoutAddress.asHexBytes()
