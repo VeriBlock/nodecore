8121b4c160ca07c2d688e4e8030ed27de6f67b5e
Bohdan Vanieiev <warchantua@gmail.com>
2020-10-14 14:01:58 +0300

    Add deserialize utils

--- veriblock-core/src/main/java/org/veriblock/core/contracts/TransactionAddress.java
+++ veriblock-core/src/main/java/org/veriblock/core/contracts/TransactionAddress.java
@@ -11,22 +11,33 @@ import org.veriblock.core.SharedConstants;
 import org.veriblock.core.bitcoinj.Base58;
 import org.veriblock.core.bitcoinj.Base59;
 import org.veriblock.core.utilities.AddressUtility;
+import org.veriblock.core.utilities.SerializerUtility;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import static org.veriblock.core.AddressConstants.ADDRESS_LENGTH;
+import static org.veriblock.core.SharedConstants.MULTISIG_ADDRESS_ID;
+import static org.veriblock.core.SharedConstants.STANDARD_ADDRESS_ID;
 
 public class TransactionAddress {
     private final String address;
+
     public String value() {
         return address;
     }
+
     private final boolean multisig;
+
     public boolean isMultisig() {
         return multisig;
     }
 
     public TransactionAddress(String address) {
-        if (address == null) throw new IllegalArgumentException("address cannot be null");
+        if (address == null) {
+            throw new IllegalArgumentException("address cannot be null");
+        }
 
         if (AddressUtility.isValidStandardAddress(address)) {
             this.address = address;
@@ -47,13 +58,27 @@ public class TransactionAddress {
         byte[] bytes = toByteArray();
 
         if (multisig) {
-            stream.write(SharedConstants.MULTISIG_ADDRESS_ID);
+            stream.write(MULTISIG_ADDRESS_ID);
         } else {
             stream.write(SharedConstants.STANDARD_ADDRESS_ID);
         }
 
         stream.write((byte) bytes.length);
         stream.write(bytes);
+    }
+
+    public static TransactionAddress deserialize(ByteBuffer stream) {
+        byte type = stream.get();
+        if (type != MULTISIG_ADDRESS_ID && type != SharedConstants.STANDARD_ADDRESS_ID) {
+            throw new IllegalArgumentException("Address type is neigher multisig, nor standard");
+        }
+
+        byte[] bytes = SerializerUtility.readSingleByteLenValue(stream, 0, ADDRESS_LENGTH);
+        if (type == STANDARD_ADDRESS_ID) {
+            return new TransactionAddress(Base58.encode(bytes));
+        }
+
+        return new TransactionAddress(Base59.encode(bytes));
     }
 
     @Override
--- veriblock-core/src/main/java/org/veriblock/core/utilities/SerializerUtility.java
+++ veriblock-core/src/main/java/org/veriblock/core/utilities/SerializerUtility.java
@@ -12,10 +12,75 @@ import org.veriblock.core.contracts.TransactionAddress;
 import org.veriblock.core.contracts.TransactionAmount;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class SerializerUtility {
+    // returns new array of size at least 'size'. if val.len < size, prepends zeroes
+    public static byte[] pad(byte[] val, int size) {
+        checkRange(size, 1, 8, "size can be 1<=size<=8");
+        if (val.length >= size) {
+            return val;
+        }
+
+        int zeroes = size - val.length;
+
+        byte[] ret = new byte[size];
+        System.arraycopy(ret, 0, ret, zeroes, val.length);
+        return ret;
+    }
+
+    public static void checkRange(int length, int min, int max, String msg) {
+        if (length > max) {
+            throw new IllegalArgumentException(String.format("Value %d is bigger than maximum %d. Details: %s.", length, max, msg));
+        }
+
+        if (length < min) {
+            throw new IllegalArgumentException(String.format("Value %d is smaller than minimum %d. Details: %s.", length, min, msg));
+        }
+    }
+
+    public static byte[] readVariableLengthValueFromStream(ByteBuffer stream, int minLen, int maxLen) {
+        int length = Math.toIntExact(readSingleBEValue(stream, 4));
+        checkRange(length, minLen, maxLen, "Can not read varLenValue of size 4");
+        byte[] buf = new byte[length];
+        stream.get(buf);
+        return buf;
+    }
+
+    public static Long readSingleBEValue(ByteBuffer stream, int size) {
+        checkRange(size, 1, 8, "Size can be 1<=size<=8");
+        byte[] buf = readSingleByteLenValue(stream, 0, size);
+        buf = pad(buf, size);
+        ByteBuffer buffer = ByteBuffer.wrap(buf);
+        if (size == 8) {
+            return buffer.getLong();
+        }
+        if (size == 4) {
+            return (long) buffer.getInt();
+        }
+        if (size == 2) {
+            return (long) buffer.getShort();
+        }
+        if (size == 1) {
+            return (long) buffer.get();
+        }
+
+        throw new IllegalArgumentException(String.format("Invalid size=%d, can be 1,2,4,8", size));
+    }
+
+    public static byte[] readSingleByteLenValue(ByteBuffer stream, int minLen, int maxLen) {
+        byte len = stream.get();
+        checkRange(len, minLen, maxLen, "Can not read single byte len value");
+
+        byte[] buf = new byte[len];
+        stream.get(buf);
+        return buf;
+    }
+
     public static void writeVariableLengthValueToStream(OutputStream stream, int value) throws IOException {
         byte[] trimmed = Utility.trimmedByteArrayFromInteger(value);
         stream.write(trimmed.length);
@@ -30,17 +95,26 @@ public class SerializerUtility {
 
     public static void writeVariableLengthValueToStream(OutputStream stream, byte[] value) throws IOException {
         byte[] dataSize = Utility.trimmedByteArrayFromInteger(value.length);
-        stream.write((byte)dataSize.length);
+        stream.write((byte) dataSize.length);
         stream.write(dataSize);
         stream.write(value);
     }
 
     public static void writeSingleByteLengthValueToStream(OutputStream stream, byte[] value) throws IOException {
-        stream.write((byte)value.length);
+        stream.write((byte) value.length);
         stream.write(value);
     }
 
-    public static void serializeRegularTransactionToStream(OutputStream stream, Byte magicByte, byte transactionType, TransactionAddress sourceAddress, TransactionAmount sourceAmount, List<Output> outputs, long signatureIndex, byte[] data) throws IOException {
+    public static void serializeRegularTransactionToStream(
+        OutputStream stream,
+        Byte magicByte,
+        byte transactionType,
+        TransactionAddress sourceAddress,
+        TransactionAmount sourceAmount,
+        List<Output> outputs,
+        long signatureIndex,
+        byte[] data
+    ) throws IOException {
         // Magic byte is used on non-mainnet networks as replay protection
         if (magicByte != null) {
             stream.write(magicByte);
@@ -56,7 +130,7 @@ public class SerializerUtility {
         sourceAmount.serializeToStream(stream);
 
         // Write destinations
-        stream.write((byte)outputs.size());
+        stream.write((byte) outputs.size());
         for (Output _output : outputs) {
             _output.serializeToStream(stream);
         }
