df357e92a98cd454f8e24dfd4ffbb7900f778648
Ante Zebic <ante.zebic@toptal.com>
2021-01-04 17:15:54 +0100

    implement vbk theme colors refactor app component to use drawer add toolbar to operations component

--- pop-miners/altchain-pop-miner/src/main/spa-client/src/app/material/material.module.ts
+++ pop-miners/altchain-pop-miner/src/main/spa-client/src/app/material/material.module.ts
@@ -6,16 +6,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 // import { MatExpansionModule } from '@angular/material/expansion';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 // import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 // import { MatListModule } from '@angular/material/list';
 
+import { CdkScrollableModule } from '@angular/cdk/scrolling';
+
 const exportedComponents = [
+  CdkScrollableModule,
   MatButtonModule,
   MatButtonToggleModule,
   MatDialogModule,
@@ -24,12 +30,15 @@ const exportedComponents = [
   MatIconModule,
   // MatInputModule,
   // MatListModule,
+  MatMenuModule,
   MatPaginatorModule,
   // MatProgressSpinnerModule,
   MatSelectModule,
+  MatSidenavModule,
   MatSnackBarModule,
   MatTableModule,
   MatToolbarModule,
+  MatTooltipModule,
 ];
 
 @NgModule({
