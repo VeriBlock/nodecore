15aaea1a4913dfcf0c05a643839d5b835275d248
BernatCarbo <41543253+BernatCarbo@users.noreply.github.com>
2021-04-17 07:39:27 +0200

    normalize eth hash (#270)
    * normalize eth hash
    
    * SPV peer fix

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/util/Extensions.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/util/Extensions.kt
@@ -8,6 +8,7 @@ internal fun ByteBuffer.getBytes(count: Int): ByteArray {
     return result
 }
 
-internal  fun String.asEthHexInt() = drop(2).toInt(16)
-internal  fun String.asEthHexLong(): Long = drop(2).toLong(16)
-internal  fun Int.asEthHex(): String = "0x${Integer.toHexString(this)}"
+internal fun String.asEthHexInt() = drop(2).toInt(16)
+internal fun String.asEthHexLong(): Long = drop(2).toLong(16)
+internal fun Int.asEthHex(): String = "0x${Integer.toHexString(this)}"
+internal fun String.normalizeEthHash() = replaceFirst("0x", "")
