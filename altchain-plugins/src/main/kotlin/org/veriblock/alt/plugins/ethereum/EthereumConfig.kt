6dd0cbb4cc303b9d32b44e805114fed813cac270
Pere Casafont <pere@pcasafont.net>
2021-05-19 10:43:17 +0200

    Fixed round indices for ETC

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/ethereum/EthereumConfig.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/ethereum/EthereumConfig.kt
@@ -15,7 +15,13 @@ class EthereumConfig(
     override val neededConfirmations: Int = 10,
     override val spFinalityDelay: Int = 100,
     override val payoutDelay: Int = 400,
-    override val blockRoundIndices: List<Int> = listOf(4, 2, 3, 1, 2),
+    override val blockRoundIndices: List<Int> = (0..keystonePeriod).map {
+        if (it == 0) {
+            4
+        } else {
+            it % 3 + 1
+        }
+    },
     override val autoMineRounds: MutableSet<Int> = HashSet(),
     override val payoutDetectionType: PayoutDetectionType = PayoutDetectionType.BALANCE_DELTA,
     override val explorerBaseUrls: ExplorerBaseUrls,
@@ -31,7 +37,13 @@ class EthereumConfig(
         configuration.neededConfirmations ?: 10,
         configuration.spFinalityDelay ?: 100,
         configuration.payoutDelay ?: 400,
-        configuration.blockRoundIndices ?: listOf(4, 2, 3, 1, 2),
+        configuration.blockRoundIndices ?: (0..(configuration.keystonePeriod ?: 40)).map {
+            if (it == 0) {
+                4
+            } else {
+                it % 3 + 1
+            }
+        },
         configuration.autoMineRounds.toMutableSet(),
         configuration.payoutDetectionType,
         configuration.explorerBaseUrls,
