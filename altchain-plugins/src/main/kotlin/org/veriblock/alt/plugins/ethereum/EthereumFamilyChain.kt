662daedd308e78f72b74b217195eaf0dd22e407e
BernatCarbo <bernatgarciacarbo@gmail.com>
2021-05-24 18:18:36 +0200

    Eth plugin vtb deserialization fix

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/ethereum/EthereumFamilyChain.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/ethereum/EthereumFamilyChain.kt
@@ -126,7 +126,7 @@ class EthereumFamilyChain(
                 endorsedBy = popBlock?.pop?.state?.endorsedBy ?: listOf(),
                 knownVbkHashes = popBlock?.pop?.data?.context?.map { it.id } ?: listOf(),
                 veriBlockPublicationIds = popBlock?.pop?.data?.atvs?.map { it.id } ?: listOf(),
-                bitcoinPublicationIds = popBlock?.pop?.data?.vtbs ?: listOf()
+                bitcoinPublicationIds = popBlock?.pop?.data?.vtbs?.map { it.id } ?: listOf()
             )
         }
     }
