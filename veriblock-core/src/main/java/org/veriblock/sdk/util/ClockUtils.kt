aee9503226d431d39809d45403dc66c779c228ab
Bohdan <warchantua@gmail.com>
2021-12-22 10:32:27 +0200

    Log time when out of sync (#371)

--- veriblock-core/src/main/java/org/veriblock/sdk/util/ClockUtils.kt
+++ veriblock-core/src/main/java/org/veriblock/sdk/util/ClockUtils.kt
@@ -8,12 +8,20 @@ import kotlinx.coroutines.time.delay
 import kotlinx.coroutines.time.withTimeout
 import org.veriblock.core.utilities.createLogger
 import org.veriblock.sdk.models.Constants
+import java.text.SimpleDateFormat
 import java.time.Duration
+import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.math.abs
 import kotlin.system.exitProcess
 
+
 private val logger = createLogger {}
+
+fun getDate(dateInMilliseconds: Long, dateFormat: String): String? {
+    val formatter = SimpleDateFormat(dateFormat);
+    return formatter.format(Date(dateInMilliseconds))
+}
 
 suspend fun checkSystemClock() = try {
     withTimeout(Duration.ofSeconds(30)) {
@@ -24,8 +32,12 @@ suspend fun checkSystemClock() = try {
             delay(Duration.ofSeconds(1))
             ntpCurrentTime = kronosClock.getCurrentNtpTimeMs()
         }
+        val systemTime = System.currentTimeMillis()
         if (abs(ntpCurrentTime - System.currentTimeMillis()) > TimeUnit.SECONDS.toMillis(Constants.ALLOWED_TIME_DRIFT.toLong())) {
-            logger.error { "The system clock is out of synchronization, please synchronize it" }
+            val format = "dd/MM/yyyy hh:mm:ss"
+            val st = getDate(systemTime, format)
+            val nt = getDate(ntpCurrentTime, format)
+            logger.error { "The system clock is out of sync. System time=${st}, NTP time=${nt}, local time exceeded allowed time drift=${Constants.ALLOWED_TIME_DRIFT}s" }
             exitProcess(1)
         } else {
             logger.debug { "The system clock is synchronized" }
