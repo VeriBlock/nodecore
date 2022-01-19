9f6d4821baade0554ccc7457894df90635a026c7
Pere Casafont <pere@pcasafont.net>
2022-01-19 02:05:47 +0100

    Clearer error when the altchain sends a not parsable response

--- altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/HttpSecurityInheritingChain.kt
+++ altchain-plugins/src/main/kotlin/org/veriblock/alt/plugins/HttpSecurityInheritingChain.kt
@@ -1,9 +1,11 @@
 package org.veriblock.alt.plugins
 
+import com.google.gson.reflect.TypeToken
 import io.ktor.client.HttpClient
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.http.content.*
+import java.lang.reflect.Type
 import mu.KLogger
 import org.veriblock.alt.plugins.util.JsonRpcRequestBody
 import org.veriblock.alt.plugins.util.NullResultException
@@ -24,11 +26,17 @@ suspend inline fun <reified T> HttpSecurityInheritingChain.rpcRequest(
 ): T {
     val jsonBody = JsonRpcRequestBody(method, params, version).toJson()
     requestsLogger?.info { "-> ${jsonBody.take(10_000)}" }
-    val response: RpcResponse = httpClient.post(config.host) {
+    val rawResponse: String = httpClient.post(config.host) {
         // Since jsonBody is a string, we have to specify it is Json content type
         body = TextContent(jsonBody, contentType = ContentType.Application.Json)
     }
-    requestsLogger?.info { "<- ${response.toJson().take(10_000)}" }
+    requestsLogger?.info { "<- ${rawResponse.take(10_000)}" }
+    val response = try {
+        val type: Type = object : TypeToken<RpcResponse>() {}.type
+        rawResponse.fromJson<RpcResponse>(type)
+    } catch (e: Exception) {
+        error("Unable to parse Altchain (${config.host}) Rpc Response from json: $rawResponse")
+    }
     return response.handle(jsonBody)
 }
 
